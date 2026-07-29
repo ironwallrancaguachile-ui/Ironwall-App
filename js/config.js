93ae60eeeabc439e2ea69be8e6f0b48062a1cf21
ironwallrancaguachile-ui <ironwallrancagua.chile@gmail.com>
2026-07-29 01:16:53 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+
+    API: {
+
+        CLIENTES:
+        "https://script.google.com/macros/s/AKfycbz7SvOi2JmjpRtLACJNVvzFtWKKeo9wUitjriZjW-JgNqPeFVliydyVy3NuNT6yiOcRbQ/exec",
+
+        FINANZAS:
+        "https://script.google.com/macros/s/AKfycbx29m90hiJKT3UKP2uJCJSm5XcEfjWwCcMYokLnw54RBTpyGLierukwYNYdCJxCiEaKng/exec",
+
+        INVENTARIO:
+        "https://script.google.com/macros/s/AKfycbxZyZAGtj2ifZ9lmhwX__tTVdydaYManjO2_Ff_ScIYtV5vpsFkQHzzKaX7ADcVXOysSw/exec",
+
+        INCIDENTES:
+        "https://script.google.com/macros/s/AKfycbzFsCtTBbmVm9fbG-subMfxddgWxWQi5rsbadTZHTIWATGQxW-xEtfLO-6QuwBK-XhRWA/exec",
+
+        DASHBOARD:
+        "PEGAR_AQUI_URL_DASHBOARD"
+
+    }
+
+};
